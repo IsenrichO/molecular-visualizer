cf7add55e1778857881005e626a57bba9411e6b7
Oliver Isenrich <IsenrichO@gmail.com>
2016-10-11 02:38:32 -0700

    init: Create base stylesheet, chemical elements JSON data file

--- app/app.js
+++ app/app.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import PeriodicTable from './components/periodic_table_component.jsx';
+
+
 export default class App extends Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <PeriodicTable />
+      </div>
     );
   }
 };
---
+++ app/components/periodic_table_component.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class PeriodicTable extends Component {
+  render() {
+    return (
+      <table>
+
+      </table>
+    );
+  }
+};
